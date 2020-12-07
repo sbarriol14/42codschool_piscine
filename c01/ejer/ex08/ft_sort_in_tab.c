5ec0190f5c83d27cac2c7a1c0fce89ff7e5ca04c
sbarriol14 <59832582+sbarriol14@users.noreply.github.com>
2020-12-07 12:09:13 +0100

    Add files via upload

---
+++ c01/ejer/ex08/ft_sort_in_tab.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <limits.h>
+
+void 	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int i;
+	int j;
+	int min;
+	int *tab2;
+
+	i = 0;
+	min = INT_MAX;
+	*tab2 = *tab;
+	while (j < size)
+	{
+		while (i < size)
+		{
+			if (tab2[i])
+				min = tab2[i];
+			i++;
+		}
+		tab[j] = min;
+		j++;
+	}
+
+
+}
