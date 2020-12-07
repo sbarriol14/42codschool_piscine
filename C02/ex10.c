473f4391a2be4aa29d19811f70ee0548e19039af
sbarriol14 <59832582+sbarriol14@users.noreply.github.com>
2020-12-07 16:14:57 +0100

    Update ex10.c

--- C02/ex10.c
+++ C02/ex10.c
@@ -3,11 +3,12 @@ unsigned int ft_strlcpy(char *dest,char *src,unsigned int size)
 {
 	int i;
 
-		i = 0;
+	i = 0;
 
-		while(i < size)
-		{
-			dest[i]=src[i];
-			i++;
-		}
+	while(i < size)
+	{
+		dest[i]=src[i];
+		i++;
+	}
+	return (i)
 }
