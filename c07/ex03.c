f4558ae90b0f94ebcb74b40d03fa5f5836915331
sbarriol14 <59832582+sbarriol14@users.noreply.github.com>
2020-12-16 00:51:39 +0100

    Update ex03.c

--- c07/ex03.c
+++ c07/ex03.c
@@ -1,11 +1,44 @@
 
 #include <stdlib.h>
 
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int		i;
+	int		j;
+	int		sizesrc;
+	int		sizedest;
+
+	sizedest = ft_strlen(dest);
+	sizesrc = ft_strlen(src);
+	i = 0;
+	j = sizedest;
+	while ((i < sizesrc))
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[sizedest + sizesrc] = '\0';
+	return (dest);
+}
+
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
 	int i;
 	char *cat;
-	cat = (char)malloc(sizeof(strs[0]) * size);
+	cat = (char**)malloc(sizeof(char*) * size);
 
 	if (size <= 0)
 	{
@@ -14,11 +47,12 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	}
 	else
 	{
-		i = 0;
-		while (strs[i] !=  0)
+		cat = strs[0];
+		i = 1;
+		while (i < size)
 		{
-			if (i != 0)
-				cat = ft_strcat(cat, sep);
+
+			cat = ft_strcat(cat, sep);
 			cat = ft_strcat(cat, strs[i]);
 			i++;
 		}
