22ba52d23dcaea7e25254ef99a9481dee6f9f242
sbarriol14 <59832582+sbarriol14@users.noreply.github.com>
2020-12-16 16:14:02 +0100

    Update ex01.c

--- c06/ex01.c
+++ c06/ex01.c
@@ -1,14 +1,20 @@
 
+
 #include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_putstr(char *str)
 {
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while(str[i] != '\0')
 	{
-		write(1, str[i], 1);
+		ft_putchar(str[i]);
 		i++;
 	}
 }
@@ -21,7 +27,7 @@ int		main(int argc, char **argv)
 	while (i < argc)
 	{
 		ft_putstr(argv[i]);
-		write (1, '\n', 1);
+		ft_putchar('\n');
 		i++;
 	}
 	return (0);
